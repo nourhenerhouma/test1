349c4fd9e036b54f5cbbc70303a65b765702837d
Nourhene <nourhene.rhouma@yahoo.com>
2026-06-10 17:14:19 -0500

    Fix allowed hosts for Render

--- test1/settings.py
+++ test1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!oje=o#eaq5r78++1)rc&td$yc3n2ihm%6t1+t7bhdwk^kzp(d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nourhene-rhouma.onrender.com']
 
 
 # Application definition
