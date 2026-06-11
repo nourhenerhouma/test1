9a46429489e79041d3a823443438916a1a0cf54f
Nourhene <nourhene.rhouma@yahoo.com>
2026-06-11 13:31:17 -0500

    Update portfolio design and static files

--- test1/settings.py
+++ test1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!oje=o#eaq5r78++1)rc&td$yc3n2ihm%6t1+t7bhdwk^kzp(d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'nourhene-rhouma.onrender.com',  # Use your current Render URL
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'myapp1',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -52,12 +51,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-]
+    ]
+    
 
 ROOT_URLCONF = "test1.urls"
 
-INTERNAL_IPS = ["127.0.0.1"]
 
 
 TEMPLATES = [
@@ -131,6 +129,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'myapp1' / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
